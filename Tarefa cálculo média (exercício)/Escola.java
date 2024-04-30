2a15e3704d7990bfc466818ef3722b3f2287b989
Ítalo Souza <italokira03@gmail.com>
2024-04-30 07:18:17 -0400

    Envio do projeto, de exercío do módulo 8.

---
+++ Tarefa cálculo média (exercício)/Escola.java
@@ -0,0 +1,14 @@
+public class Escola {
+    public static void main(String args[]) {
+    
+        int notaAluno1 = 7;
+        int notaAluno2 = 5;
+        int notaAluno3 = 3;
+        int notaAluno4 = 2;
+
+        int somaDasNotas = notaAluno1+notaAluno2+notaAluno3+notaAluno4;
+        int médiaDasNotas = somaDasNotas / 4;
+
+        System.out.println("A média das notas é: " + médiaDasNotas+" pontos.");
+    }
+}
