7dfd02b16b210206015feef85763d779df61afaa
Ítalo Souza <italokira03@gmail.com>
2024-05-05 08:40:16 -0400

    Envio da atividade do módulo 9

---
+++ Exercício módulo 9/exercício.java
@@ -0,0 +1,11 @@
+public class exercício {
+    public static void main(String[] args) {
+
+        int RG = 682416;
+        System.out.println(RG);
+
+        Integer RG1 = RG;
+        System.out.println(RG1);
+        
+    }
+}
