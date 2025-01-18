74e358d7f4c893f4a76d7738abd631184890d4fa
Ítalo Souza <italokira03@gmail.com>
2025-01-18 06:29:52 -0400

    Envio da tarefa do módulo 18

---
+++ Exercíco anotations (Mod 18)/src/OBJ/Pessoa.java
@@ -0,0 +1,35 @@
+package OBJ;
+
+import Anotation.Tabela;
+
+
+public class Pessoa {
+
+    private String nome;
+    private byte idade;
+
+    @Tabela()
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, byte idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public byte getIdade() {
+        return idade;
+    }
+
+    public void setIdade(byte idade) {
+        this.idade = idade;
+    }
+}
