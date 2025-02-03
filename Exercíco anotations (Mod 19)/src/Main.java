52cf353f1f936802f0404583bde932bdcf747742
Ítalo Souza <italokira03@gmail.com>
2025-02-03 07:19:12 -0400

    Envio do exercício do módulo 19

--- Exercíco anotations (Mod 19)/src/Main.java
+++ Exercíco anotations (Mod 19)/src/Main.java
@@ -3,14 +3,12 @@ import OBJ.Pessoa;
 
 import javax.xml.transform.Source;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
     try {
         Class<?> classe = Class.forName("Pessoa");
 
-        if(classe.isAnnotationPresent(Tabela.class);
+        if(classe.isAnnotationPresent(Tabela.class));
 
         Tabela tabela = classe.getAnnotation(Tabela.class);
 
